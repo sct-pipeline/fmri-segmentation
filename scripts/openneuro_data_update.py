ae111c5d095bc31f8392dc498372cdb1cb68d545
Rohan Banerjee <banerjee.rohan98@gmail.com>
2024-05-30 11:37:21 -0400

    openneuro dataset update WIP

--- scripts/openneuro_data_update.py
+++ scripts/openneuro_data_update.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import datetime
+import json
 
 # step 1: takes a yml files as the input which contains all the names of the subjects which were manually corrected
 # step 2: This copies the segmentation files from the nnUNet dataset and copies it to the derivatives/labels folder in the openneuro dataset
@@ -33,25 +34,31 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Update dataset with segmentation files')
-    parser.add_argument('nnunet_dataset', type=str, help='Path to nnUNet dataset')
-    parser.add_argument('updated_dataset', type=str, help='Path to dataset to be updated')
+    parser.add_argument('-nnunet-dataset', type=str, help='Path to nnUNet dataset')
+    parser.add_argument('-openneuro-dataset', type=str, help='Path to openneuro dataset')
+    parser.add_argument('-corrected-subjects', type=str, help='Path to input file containing subjects')
     args = parser.parse_args()
 
     nnunet_dataset = args.nnunet_dataset
-    updated_dataset = args.updated_dataset
+    openneuro_dataset = args.openneuro_dataset
+    input_file = args.corrected_subjects
 
-    subjects = read_subjects_from_yaml('input.yml')
+    subjects = read_subjects_from_yaml(input_file)
 
     # Step 2: Copy the segmentation files to the derivatives/labels folder
     for subject in subjects:
-        source_path = f'{nnunet_dataset}/{subject}/segmentation.nii.gz'
-        destination_path = f'{updated_dataset}/derivatives/labels/{subject}_seg.nii.gz'
+        #todo: add a arguement to get the nnUnet dataset prefix, I am currently assuming it to be spinefmri (as per my dataset)
+        source_path = f'{nnunet_dataset}/labelsTr/spinefmri_{subject}.nii.gz'
+        #logic: remove the already existing segmentation file and copy the manually segmented one in place of it
+        destination_path = f'{openneuro_dataset}/derivatives/labels/{subject}_task-*_desc-spinalcordmask.nii.gz'
+        task = destination_path.split('_task-')[1].split('_desc')[0]
+        if os.path.exists(destination_path):
+            os.remove(destination_path)
         shutil.copyfile(source_path, destination_path)
-        # Step 2.1: Rename the files as per the updated dataset
-        os.rename(destination_path, f'{updated_dataset}/derivatives/labels/{subject}.nii.gz')
+        os.rename(destination_path, f'{openneuro_dataset}/derivatives/labels/{subject}_task-{task}_desc-spinalcordmask.nii.gz')
 
     # Step 3: Update the JSON files linked to the dataset
-    json_file_path = f'{updated_dataset}/dataset_description.json'
+    json_file_path = f'{openneuro_dataset}/dataset_description.json'
     with open(json_file_path, 'r') as file:
         json_data = json.load(file)
     json_data['GeneratedBy'] = [
