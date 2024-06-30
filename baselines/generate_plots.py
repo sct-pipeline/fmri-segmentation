56a45db2d731f23aeef3c8aa40a9a6800e524a30
rohanbanerjee <banerjee.rohan98@gmail.com>
2024-06-30 13:53:07 -0400

    minor updates

--- baselines/generate_plots.py
+++ baselines/generate_plots.py
@@ -13,8 +13,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+import ptitprince as pt
+import argparse
+import matplotlib.patches as mpatches
 
-csv_files = ['/home/GRAMES.POLYMTL.CA/robana/duke/temp/rohan/fmri_sc_seg/datasets/held-out_test_common_bids_all_manual_data/results_all_manual.csv', '/home/GRAMES.POLYMTL.CA/robana/duke/temp/rohan/fmri_sc_seg/datasets/held-out_test_common_bids_resunet/results_resunet.csv', '/home/GRAMES.POLYMTL.CA/robana/duke/temp/rohan/fmri_sc_seg/datasets/held-out_test_common_bids_all_manual_data/results_all_manual.csv', '/home/GRAMES.POLYMTL.CA/robana/duke/temp/rohan/fmri_sc_seg/datasets/held-out_test_common_bids/results_contrast_agnostic.csv']
+parser = argparse.ArgumentParser(description='Process results CSV files.')
+parser.add_argument('--csv_files', metavar='CSV', type=str, nargs='+')
+args = parser.parse_args()
+csv_files = args.csv_files
 
 dice_scores = []
 hausdorff_distances = []
@@ -27,17 +33,33 @@ for csv_file in csv_files:
     dice_scores.extend(df['Dice Score (Mean ± Std Dev)'].str.split('\\+\\-').str[0].astype(float))
     hausdorff_distances.extend(df['Hausdorff Distance (Mean ± Std Dev)'].str.split('\\+\\-').str[0].astype(float))
 
-    model_name = os.path.basename(csv_file).replace('.csv', '')
+    model_name = os.path.basename(csv_file).replace('.csv', '').split('_')[-1]
     models.extend([model_name]*len(df))
 
 
 df_plot = pd.DataFrame({'Model': models, 'Dice Score': dice_scores, 'Hausdorff Distance': hausdorff_distances})
 
 
-plt.figure(figsize=(10, 6))
-sns.violinplot(x='Model', y='Dice Score', data=df_plot, hue='Model')
-plt.savefig('/home/GRAMES.POLYMTL.CA/robana/duke/temp/rohan/fmri_sc_seg/plots/test_d_multiple.png')
 
+# Plot Dice Score
 plt.figure(figsize=(10, 6))
-sns.violinplot(x='Model', y='Hausdorff Distance', data=df_plot, hue='Model')
-plt.savefig('/home/GRAMES.POLYMTL.CA/robana/duke/temp/rohan/fmri_sc_seg/plots/test_h_multiple.png') 
+sns.set_theme(style="darkgrid")
+rain = pt.RainCloud(x='Model', y='Dice Score', data=df_plot,palette="Set2", bw=.2, linewidth = 2, width_viol=.7, orient="v", alpha=1.0, point_size = 4, dodge = False, pointplot=True, linecolor = 'purple')
+
+# # Customize legend to show unique model names
+# handles, labels = rain.get_legend_handles_labels()
+# by_label = dict(zip(labels, handles))
+# plt.legend(by_label.values(), by_label.keys(), loc='lower right')
+
+plt.savefig('/home/GRAMES.POLYMTL.CA/robana/duke/temp/rohan/fmri_sc_seg/plots/methods_comparison_dice.png', bbox_inches='tight')
+
+
+# Plot Hausdorff Distance
+plt.figure(figsize=(10, 6))
+rain = pt.RainCloud(x='Model', y='Hausdorff Distance', data=df_plot,palette="Set2", bw=.2, linewidth = 1, width_viol=.7, orient="v", alpha=1.0, point_size = 4, dodge = False, pointplot=True, linecolor = 'purple')
+
+# Customize legend to show unique model names
+handles, labels = rain.get_legend_handles_labels()
+by_label = dict(zip(labels, handles))
+plt.legend(by_label.values(), by_label.keys(), loc='upper right')
+plt.savefig('/home/GRAMES.POLYMTL.CA/robana/duke/temp/rohan/fmri_sc_seg/plots/methods_comparison_hausdorff.png', bbox_inches='tight') 
